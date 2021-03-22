0f3f5898463010c51fd1f93313150788175b992b
Cody Lillywhite <codylillyw@gmail.com>
2021-03-22 12:18:33 -0400

    fix next button

--- script.js
+++ script.js
@@ -3,7 +3,8 @@ timeLineItems.forEach(element => {
     element.addEventListener("click",() => {
         toggleInfoVisibilityOn(document.getElementById(element.id + '-info'));
         toggleCircleSelection(element);
-    })
+        nextElementIndex = parseInt(element.dataset.index)+1;
+    });
 });
 var nextElementIndex = 0;
 document.getElementById("next-timeline-item-button").addEventListener("click", () => { 
